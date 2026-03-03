226dc4c8997b633e8de4ccf62b0c34b0a7767840
PeiyiXiong <pxiong2@uci.edu>
2026-03-02 21:41:42 -0800

    hint

--- final aaa/Assets/scripts/PlayerMovement.cs
+++ final aaa/Assets/scripts/PlayerMovement.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 6f;
-    public float mouseSensitivity = 500f;
+    public float mouseSensitivity = 150f;
     public float gravity = -9.81f;
     public float jumpHeight = 1f;
 
     private CharacterController cc;
-    private float xRotation = 0f;
+    private float xRotation = 0f; 
     private Transform mainCamera;
     private Vector3 velocity;
 
@@ -38,12 +38,9 @@ public class PlayerMovement : MonoBehaviour
     private void HandleCameraRotation()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        mainCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
+        mainCamera.localRotation = Quaternion.Euler(0f, 0f, 0f);
     }
 
     private void HandleGravityAndJump()
