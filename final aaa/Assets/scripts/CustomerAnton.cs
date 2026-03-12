f43c439517463257f76b83de352420890716cb20
ruixuanp1122 <ruixuanp@uci.edu>
2026-03-11 22:52:25 -0700

    Anton Dialogue

--- final aaa/Assets/scripts/CustomerAnton.cs
+++ final aaa/Assets/scripts/CustomerAnton.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class CustomerAnton : InteractableBase
 {
-    public DialogueNode welcomeNode;
-    public DialogueNode thankYouNode;
+    public DialogueNode nothingNode;
+    public DialogueNode burgerNode;
+    public DialogueNode pizzaNode;
     public HamburgerInteract hamburgerItem;
+    public PizzaInteract pizzaItem;
 
     public override void Interact()
     {
         if (hamburgerItem != null && hamburgerItem.isPicked)
         {
-            DialogueManager.Instance.StartDialogue(thankYouNode);
+            DialogueManager.Instance.StartDialogue(burgerNode);
             OrderManager.Instance.SubmitOrder();
             DeliveryManager.Instance.CompleteFirstDelivery();
         }
+
+        else if (pizzaItem != null && pizzaItem.isPicked)
+        {
+            DialogueManager.Instance.StartDialogue(pizzaNode);
+        }
+
         else
         {
-            DialogueManager.Instance.StartDialogue(welcomeNode);
+            DialogueManager.Instance.StartDialogue(nothingNode);
         }
     }
 }
