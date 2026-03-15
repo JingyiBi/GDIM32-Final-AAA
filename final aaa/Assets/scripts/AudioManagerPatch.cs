e576a9ca72e168ce081bc4d6d5e2262fe862940a
JingyiBi <17740878256@163.com>
2026-03-15 15:27:36 -0700

    Revert "audio"
    This reverts commit 921e8c446aea163494bacbc7e5b70211a501679e.

--- final aaa/Assets/scripts/AudioManagerPatch.cs
+++ final aaa/Assets/scripts/AudioManagerPatch.cs
@@ -3,32 +3,12 @@ using UnityEngine;
 public class AudioManagerPatch : MonoBehaviour
 {
     public static AudioManagerPatch Instance;
-
-    [Header("Audio Sources")]
-    public AudioSource bgmSource; 
-    public AudioSource sfxSource; 
-
-    [Header("Audio Clips")]
-    public AudioClip backgroundMusic; 
+    public AudioSource sfxSource;
     public AudioClip pickupClip;   
-    public AudioClip moneyClip;    
+    public AudioClip deliveryClip; 
 
-    void Awake() 
-    { 
-        Instance = this; 
-        AudioListener.pause = false;
-        AudioListener.volume = 1f;
-    }
+    void Awake() { Instance = this; }
 
-    
-    public void PlayPickup() 
-    { 
-        if(pickupClip && sfxSource) sfxSource.PlayOneShot(pickupClip); 
-    }
-
-    
-    public void PlayMoney() 
-    { 
-        if(moneyClip && sfxSource) sfxSource.PlayOneShot(moneyClip); 
-    }
+    public void PlayPickup() { if(pickupClip) sfxSource.PlayOneShot(pickupClip); }
+    public void PlayDelivery() { if(deliveryClip) sfxSource.PlayOneShot(deliveryClip); }
 }
