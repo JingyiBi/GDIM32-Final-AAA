921e8c446aea163494bacbc7e5b70211a501679e
JingyiBi <17740878256@163.com>
2026-03-15 15:16:12 -0700

    audio

--- final aaa/Assets/scripts/AudioManagerPatch.cs
+++ final aaa/Assets/scripts/AudioManagerPatch.cs
@@ -3,12 +3,32 @@ using UnityEngine;
 public class AudioManagerPatch : MonoBehaviour
 {
     public static AudioManagerPatch Instance;
-    public AudioSource sfxSource;
+
+    [Header("Audio Sources")]
+    public AudioSource bgmSource; 
+    public AudioSource sfxSource; 
+
+    [Header("Audio Clips")]
+    public AudioClip backgroundMusic; 
     public AudioClip pickupClip;   
-    public AudioClip deliveryClip; 
+    public AudioClip moneyClip;    
 
-    void Awake() { Instance = this; }
+    void Awake() 
+    { 
+        Instance = this; 
+        AudioListener.pause = false;
+        AudioListener.volume = 1f;
+    }
 
-    public void PlayPickup() { if(pickupClip) sfxSource.PlayOneShot(pickupClip); }
-    public void PlayDelivery() { if(deliveryClip) sfxSource.PlayOneShot(deliveryClip); }
+    
+    public void PlayPickup() 
+    { 
+        if(pickupClip && sfxSource) sfxSource.PlayOneShot(pickupClip); 
+    }
+
+    
+    public void PlayMoney() 
+    { 
+        if(moneyClip && sfxSource) sfxSource.PlayOneShot(moneyClip); 
+    }
 }
